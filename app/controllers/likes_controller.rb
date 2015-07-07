47b25f5d58ecf17b57f328afa72a9c88421eb094
redo2011dht <redo2011dht@gmail.com>
2015-07-07 16:00:30 +0700

    modified:   app/controllers/likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,37 +1,24 @@
 class LikesController < ApplicationController
+ # before_action :authenticate_user!,only: [:s
   before_action :set_like, only: [:show, :edit, :update, :destroy]
+   def index
+    like = Like.where({user_id: current_user.id, post_id: params[:post_id]}).first_or_create(:status => false)
+    like.status = !like.status
+    like.save
 
-   def like
-    @post=Post.find(params[:post_id])
-   # @like = @post.likes.build()
-   @like=Like.find(params[:like_id])
-    @like.user=current_user
-    @like.status=true
-    @post.likenumber=@post.likenumber.to_i+1
-    @like.save
-    redirect_to posts_url
-  end
-
-  def unlike
-    @post=Post.find(params[:post_id])
-    #@like=@post.likes.build()
-    @like.user_id=current_user.id
-    @like.status=false
-    @post.likenumber =  @post.likenumber.to_i - 1 
-    @like.save
-    redirect_to posts_url
-  end
-
-
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_like
-      @like = Like.find(params[:id])
+    post=Post.find_by_id(params[:post_id])
+    if like.status
+      session[:like]=true
+      post.likenumber+=1
+    else
+      post.likenumber-=1
+      session[:like]=false
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def like_params
-      params.require(:like).permit(:post_id)
-    end
+    post.save
+    redirect_to :back
+  end
+  private 
+  def likes_params
+    params.require(:like).permit(:status)
+  end
 end
